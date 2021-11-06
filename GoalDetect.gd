9a6c8b19a16429871fc12130b27ff16faf4b809d
Tetaes <techathats@gmail.com>
2021-11-06 18:16:32 +0700

    players + goal + wall + ice

---
+++ GoalDetect.gd
@@ -0,0 +1,14 @@
+extends Area2D
+
+var occupied = false
+
+#if players exit goal
+func _on_Goal_body_exited(body):
+	if body.is_in_group('player'):
+		occupied = false
+
+#if players enter goal
+func _on_Goal_body_entered(body):
+	if body.is_in_group('player'):
+		occupied = true
+
