088838305c58de26485d6530e8d23366f41c1891
Tetaes <techathats@gmail.com>
2021-12-03 15:07:15 +0700

    better ice

--- Player.gd
+++ Player.gd
@@ -30,48 +30,98 @@ var facing = {
 func _unhandled_input(event):
 	
 	#ignore input if tween is active
-	if tween.is_active():
-		return
+	
 		
 	#check input
 	for dir in inputs.keys():
 		if event.is_action_pressed(dir):
 			if dir == "ui_left":
 				$character.play("run_left")
-				move(dir)
+				move(dir,1)
 			elif dir == "ui_right":
 				$character.play("run_right")
-				move(dir)
+				move(dir,1)
 			elif dir == "ui_down":
 				$character.play("run_down")
-				move(dir)
+				move(dir,1)
 			elif dir == "ui_up":
 				$character.play("run_up")
-				move(dir)
+				move(dir,1)
 #move function
-func move(dir):
+func move(dir,amt):
 	
-	#find move vector
-	var move_vector = inputs[dir] * grid_size
-	ray.cast_to = move_vector
-	ray.force_raycast_update()
-	
-	#if nothing collides with vector
-	if not ray.is_colliding():
-		#move players
-		move_tween(dir,1)
-	
-	#if something collides
-	else:
-		var collider = ray.get_collider()
-		#if it's ice
-		if collider.is_in_group('ice'):
-			#move 2 spaces
-			move_tween(dir,2)
-		#if it's fire
-		elif collider.is_in_group('fire'):
-			#restart
-			get_node("/root/Game").reloadlevel(get_node("../../").get_name())
+	#try to move max blocks, then max-1, max-2, until 0
+	for blocks in range(amt,0,-1):
+		
+		#find move vector
+		var move_vector = inputs[dir] * grid_size * blocks
+		ray.cast_to = move_vector
+		ray.force_raycast_update()
+		
+		#if nothing collides with vector
+		if not ray.is_colliding():
+			#move players
+			move_tween(dir,blocks)
+			break
+		
+		#if something collides
+		else:
+			var collider = ray.get_collider()
+			
+			#if it's ice
+			if collider.is_in_group('ice'):
+				
+				#keep track of ices
+				var ice_count = 0
+				var ice_list = []
+				
+				#check if player is INSIDE the ice from a previous move
+				#by making ray very small
+				ray.cast_to = move_vector*0.1
+				ray.force_raycast_update()
+				
+				#if inside, decrease ice count by 1
+				if ray.is_colliding():
+					ice_count = -1
+				
+				#while there are more ices
+				var moreice = true
+				
+				while moreice:
+					#check 1+ice_count blocks away
+					move_vector = inputs[dir] * grid_size * (1 + ice_count)
+					ray.cast_to = move_vector
+					ray.force_raycast_update()
+					moreice = false
+					
+					#if there is ice
+					if ray.is_colliding():
+						collider = ray.get_collider()
+						if collider.is_in_group('ice'):
+							
+							#disable the ice
+							get_node("../../Ices/" + collider.get_name() + "/CollisionShape2D").set_deferred("disabled", true)
+							yield(get_tree().create_timer(0.01), "timeout")
+							
+							#keep track of ice and say the loop needs to continue
+							ice_list.append(collider.get_name())
+							ice_count += 1
+							moreice = true
+				
+				#move 1+ice_count blocks
+				move(dir,1+ice_count)
+				
+				#reanable all the ices
+				for i in ice_list:
+					get_node("../../Ices/" + i + "/CollisionShape2D").set_deferred("disabled", false)
+					
+			#if it's fire
+			elif collider.is_in_group('fire'):
+				#restart
+				get_node("/root/Game").reloadlevel(get_node("../../").get_name())
+				break
+				
+		#print(blocks)
 	
 	#rotate and squish players
 	squish_tween(dir)
