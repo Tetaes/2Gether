d5ade2221ce0e62b1372413f9306f42ca5b68423
1Pz <88140498+iPz1016@users.noreply.github.com>
2021-12-06 15:50:24 +0700

    update tileset_forest

--- Game.gd
+++ Game.gd
@@ -9,7 +9,7 @@ extends Node2D
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	#comment line 12 and 13 and uncomment line 14 to see menu
-	var level = load("res://Assets/map/map_forest/forest_5.tscn").instance()
+	var level = load("res://Assets/map/map_forest/forest_10.tscn").instance()
 	add_child(level)
 	#pass
 
